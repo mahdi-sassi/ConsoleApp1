14c2ff44ddf0354b957d8f3556a14c2ef00840df
Mahdi Sassi <m.sassi@root.sa>
2018-02-28 10:37:04 +0100

    convert IEnumerable to Array, List and average

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,13 +11,18 @@ namespace ConsoleApp1
 
             IEnumerable<int> result = from value in Enumerable.Range(0, 2)
                                       select value;
-            foreach(int element in result)
-            {
-                Console.WriteLine(element);
-            }
+            //foreach(int element in result)
+            //{
+            //    Console.WriteLine(element);
+            //    Console.ReadLine();
 
+            //}
+            double average = result.Average();
+            Console.WriteLine(average);
+            Console.Read();
 
-
+            List<int> list = result.ToList();
+            int[] array = result.ToArray();
 
             //Models model = new Models();
             //model.name = "Hello World!";
